874248f64556b0a6242483b2f04ab838493493b6
Formanek Dénes <formdenes@gmail.com>
2025-12-02 16:52:55 +0100

    disabled go plugin as it caused issues with format on save

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,7 +1,7 @@
 vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y', { desc = '[Y]ank to clipboard' })
 vim.keymap.set({ 'n', 'v' }, '<leader>p', '"+p', { desc = '[P]aste from clipboard' })
 
--- Sourceing helpers
+-- Sourcing helpers
 vim.keymap.set('n', '<leader>xf', '<cmd>source %<CR>', { desc = 'Source [F]ile' })
 vim.keymap.set('n', '<leader>xx', ':.lua<CR>', { desc = 'Source current line' })
 vim.keymap.set('v', '<leader>x', ':lua<CR>', { desc = 'Source lines' })
