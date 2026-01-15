1bd57cd3a2356788995ac458690fe9286b86de61
Formanek Dénes <formdenes@gmail.com>
2026-01-15 13:19:44 +0100

    Fixed missing return statement

--- lua/custom/plugins/markdown.lua
+++ lua/custom/plugins/markdown.lua
@@ -1,8 +1,9 @@
 -- For `plugins/markview.lua` users.
-return {
-  'OXY2DEV/markview.nvim',
-  lazy = false,
-
-  -- Completion for `blink.cmp`
-  -- dependencies = { "saghen/blink.cmp" },
-}
+-- return {
+--   'OXY2DEV/markview.nvim',
+--   lazy = false,
+--
+--   -- Completion for `blink.cmp`
+--   -- dependencies = { "saghen/blink.cmp" },
+-- }
+return {}
